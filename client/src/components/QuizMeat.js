19bbf2116d40a0412c7f5f3cb2854ba672e0cc93
Paul Hamill <enquiries@nestboxproperty.co.uk>
2022-08-21 19:27:22 +0100

    QuizMeet header updated

--- client/src/components/QuizMeat.js
+++ client/src/components/QuizMeat.js
@@ -3,7 +3,7 @@ import React from 'react'
 const QuizMeat = () => {
   return (
       <div>
-          <h2></h2>
+          <h2>What products do you eat in an average week?</h2>
           <form>
               <button>Cheese</button>
               <div>
