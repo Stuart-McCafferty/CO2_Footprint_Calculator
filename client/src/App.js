860afa9fbdcabc279f45bc3980c1d4e16b9a8412
Stuart McCafferty <sm342@hw.ac.uk>
2022-08-21 23:17:21 +0100

    Initial backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { QuizContainer } from './containers/QuizContainer';
 
 function App() {
+
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:9000/api/values')
+      .then(res => res.json())
+      .then(data => setValues(data));
+  })
+
   return (
+    <>    
     <div className="App">
-      <QuizContainer/>
+    <QuizContainer/>
     </div>
-  );
+    </>
+);    
+
 }
 
 export default App;
--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,8 +1,44 @@
 const express = require('express');
-const ObjectID = require('mongodb').ObjectID
 
-const createRouter = function (collection) {
+const createRouter = function (data) {
 
   const router = express.Router();
 
-}
+  
+  //INDEX
+  router.get('/', (req, res) => { 
+    res.json(data); 
+  });
+
+
+  //SHOW
+  router.get('/:id', (req, res) => { 
+    res.json(data[req.params.id]); 
+  });
+
+
+  // CREATE
+  router.post('/', (req, res) => { 
+    data.push(req.body); 
+    res.json(data); 
+  });
+
+
+  // UPDATE
+  router.put('/:id', (req, res) => { 
+    data[req.params.id] = req.body; 
+    res.json(data);
+  });
+
+
+  // DELETE
+  router.delete('/:id', (req, res) => { 
+    data.splice(req.params.id, 1); 
+    res.json(data); 
+  });
+
+  return router;
+
+};
+
+module.exports = createRouter;
