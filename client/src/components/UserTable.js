d23564bc312b3205420e2a634c7aaa22dcfa71e6
Nick Murray <nicholas.murray1332@googlemail.com>
2022-08-22 23:24:18 +0100

    trying to connect mongodb

--- client/src/components/UserTable.js
+++ client/src/components/UserTable.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
-import NavBar from '../components/NavBar';
 
 const UserTable = () => {
     
   return (
-    <div>UserTable</div>
+    <div>
+        User Table
+    </div>
   )
 }
 
