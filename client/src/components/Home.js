a96ee367ee0ae3f68d37085f3ee913e8ded827c7
Paul Hamill <enquiries@nestboxproperty.co.uk>
2022-08-22 18:54:51 +0100

    user input form and home page file added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,23 +3,12 @@ import QuizContainer from "./containers/QuizContainer";
 import 'survey-core/modern.min.css';
 import { StylesManager, Model } from 'survey-core';
 
-
 StylesManager.applyTheme("modern");
-
-// function Home(){
-//   const [values, setValues] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:9000/api/values')
-  //     .then(res => res.json())
-  //     .then(data => setValues(data));
-  // })
 
   Survey.StylesManager.applyTheme("default");
   var json = {
     completeText: "Finish",
-    pageNextText: "Continue",
-    pagePrevText: "Previous",
+    pageNextText: "Start",
 
   return (
     <>    
@@ -27,12 +16,10 @@ StylesManager.applyTheme("modern");
       <h1>Carbon Hero</h1>
       <h2>Lets help save the planet and calculate your carbon footprint</h2>
       <p>The test takes approximately 5 minutes to complete</p>
-      <p>The questions are divided in 3 sections: Transport, Food Consumption and Travel, </p>
-      <button>Start</button>
+      <p>The questions are divided in 3 sections, Transport Food Consumption and Travel, </p>
     </div>
     </>
-    );  
+)};
 
-};
 
 export default App;
