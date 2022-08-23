177658d5384bad76db39710c672aef9b7b97a032
Stuart-McCafferty <Stuartj678@gmail.com>
2022-08-23 16:19:05 +0100

    fixed repeating question bug

--- client/src/containers/ImprovementContainer.js
+++ client/src/containers/ImprovementContainer.js
@@ -4,7 +4,7 @@ const ImprovementContainer = ({}) => {
     // TRANSPORT - transport >= bus recommend walking if i equals walking congrulate them say well done 
     // FOOD - foodcounter > X say you can improve.... detail what they can else ssay well done 
     // TRAVEL - travelcounter > X say you should decrease your long flights and do more staycations else say well done 
-
+    
 
     return (
         <p>test</p>
