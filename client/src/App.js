85b0008d927e42d68a7e258547857daf30197c63
“lawriecarly” <Lawrie.davidson91@gmail.com>
2022-08-23 15:10:02 +0100

    sortedcComponents

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import HighchartsReact from 'highcharts-react-official';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import NavBar from "./components/NavBar";
 import { getUsers } from './components/QuizService';
+import { HomePage } from "./components/HomePage";
 
 
 function App(){
@@ -45,35 +46,12 @@ function App(){
   return (
 
     <>
-    <HomepageContainer>
-      <NavBarContainer>
-        <SiteHeading>CO2 CALCULATOR</SiteHeading>
-        <AnimationContainer>
-          {/* <Lottie options={defaultOptions} height={200} width={200} /> */}
-        </AnimationContainer>
-        <NavLinks>
-          <Link>Quiz</Link>
-          <Link>Results</Link>
-          <Link>Improvements</Link>
-        </NavLinks>
-      </NavBarContainer>
-        <HeaderContainer>
-          <H1>CHALLENGE YOURSELF TO MAKE A DIFFERENCE</H1>
-          <Para>The Climate Crisis is here, what are you going to do about it?x</Para>
-        </HeaderContainer>
-        <BodyContainer>
-          <SurveyContainer>
-            <QuizContainer/>
-          </SurveyContainer>
-        </BodyContainer>
-
-    </HomepageContainer>
 
 <Router>
-{/* <NavBar/> */}
+<NavBar/>
   <Routes>
-      {/* <Route path="/" element={<Home/>}/> */}
-      <Route path="/quiz" element={<QuizContainer/>}/>
+      <Route path="/" element={<HomePage/>}/>
+      {/* <Route path="/quiz" element={<QuizContainer/>}/> */}
       <Route path="/table" element={<UserContainer users={users}/>}/>
   </Routes>
 </Router>
@@ -84,78 +62,3 @@ function App(){
 
 };
 export default App;
-
-const HomepageContainer = styled.main`
-    background-color: #F9F5F0;
-`
-
-const NavBarContainer = styled.nav`
-  padding-left: 50px;
-  padding-right: 50px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 120px;
-  background-color: #0A1128;
-  border-bottom: solid black 3px;
-
-`
-const NavLinks = styled.ul`
-  color: white;
-  list-style-type: none;
-`
-const Link = styled.li`
-  display: inline;
-  margin-right: 10px;
-  font-weight: bold;
-
-`
-
-const HeaderContainer = styled.div`
-  padding-top: 50px;
-  padding-left: 150px;
-  padding-right: 150px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 30px;
-`
-
-const SiteHeading = styled.h1`
-  color: white ;
-  letter-spacing: 3px;
-  font-family: 'Covered By Your Grace';
-  font-family: 'Rubik Dirt';
-`
-
-const BodyContainer = styled.div`
-  padding: 50px;
-`
-
-const H1 = styled.h1`
-    font-family: 'Covered By Your Grace';
-    font-family: 'Rubik Dirt';
-`
-
-const Para = styled.p`
-  font-family: 'Karla', sans-serif;
-  font-size: 24px;
-`
-
-
-const SurveyContainer = styled.div`
-  background-color: white;
-  margin-left: 100px;
-  margin-right: 100px;
-  border: solid 1px #F7EDE2;
-  box-shadow: 5px 5px 5px 5px #0F6B59;
-`
-
-const AnimationContainer = styled.div `
-  position: absolute;
-  width: 200px;
-  height: 200px;
-  top: 1%;
-  left: 50%;
-  margin-left: -100px; /* Negative half of width. */
-`
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const NavBar = () => {
 
-    return (
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-                <Link to="/quiz">Quiz</Link>
-            <li>
-                <Link to="/table">Table</Link>
-            </li>
-        </ul>
-    );
+return (
+
+    <NavBarContainer>
+        <SiteHeading>CO2 CALCULATOR</SiteHeading>
+          {/* <Lottie options={defaultOptions} height={200} width={200} /> */}
+        <NavLinks>
+        <Link to="/">Home</Link>
+        <Link to="/quiz">Quiz</Link>
+        <Link to="/table">Table</Link>
+        </NavLinks>
+    </NavBarContainer>
+);
 }
 
 export default NavBar;
+
+
+const NavBarContainer = styled.nav`
+  padding-left: 50px;
+  padding-right: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 120px;
+  background-color: #0A1128;
+  border-bottom: solid black 3px;
+
+`
+const NavLinks = styled.ul`
+  color: white;
+  list-style-type: none;
+`
+
+const SiteHeading = styled.h1`
+  color: white ;
+  letter-spacing: 3px;
+  font-family: 'Covered By Your Grace';
+  font-family: 'Rubik Dirt';
+`
+// const Link = styled.li`
+//   display: inline;
+//   margin-right: 10px;
+//   font-weight: bold;
+
+// `
