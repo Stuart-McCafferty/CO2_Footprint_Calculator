dc70694c633304829a9d818723a2dd0eefd667a6
“lawriecarly” <Lawrie.davidson91@gmail.com>
2022-08-23 14:44:38 +0100

    addedGraph

--- server/db/user_seeds.js
+++ server/db/user_seeds.js
@@ -5,41 +5,65 @@ db.app_users.insertMany([
     {
         name: "Andy",
         location: "Edinburgh",
+        transport: 4.3,
+        food: 198,
+        travel: 631,
         score: 831.5
     },
     {
         name: "Hideo",
         location: "Tokyo",
+        transport: 2,
+        food: 232,
+        travel: 298,
         score: 532
     },
     {
         name: "David",
         location: "London",
+        transport: 7,
+        food: 174,
+        travel: 200,
         score: 441
     },
     {
         name: "Jayson",
         location: "Boston, MA",
+        transport: 10,
+        food: 298,
+        travel: 430,
         score: 742.5
     },
     {
         name: "Marie",
         location: "Paris",
+        transport: 12,
+        food: 400,
+        travel: 224,
         score: 664
     },
     {
         name: "Chen",
         location: "Beijing",
+        transport: 15,
+        food: 78,
+        travel: 309,
         score: 502
     },
     {
         name: "Monica",
         location: "Rome",
+        transport: 3.7,
+        food: 402,
+        travel: 232,
         score: 792
     },
     {
         name: "Don",
         location: "Madrid",
+        transport: 3,
+        food: 233,
+        travel: 144,
         score: 958.5
     }
 ])
